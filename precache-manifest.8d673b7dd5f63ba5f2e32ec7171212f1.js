2ddbed2f99a02fc81e5148551fc5164a34e34c0c
Dhruvit <vachhanidhruvit@gmail.com>
2020-12-12 20:15:20 +0530

    Updates

---
+++ precache-manifest.8d673b7dd5f63ba5f2e32ec7171212f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9cd1f1978202c8336c8c34282e7b8b",
+    "url": "/tutorialweb/index.html"
+  },
+  {
+    "revision": "ec60814886a0dd4fae14",
+    "url": "/tutorialweb/static/css/main.03b83020.chunk.css"
+  },
+  {
+    "revision": "6ac931e311c7d322703e",
+    "url": "/tutorialweb/static/js/2.f3da33ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tutorialweb/static/js/2.f3da33ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec60814886a0dd4fae14",
+    "url": "/tutorialweb/static/js/main.a76a4fcf.chunk.js"
+  },
+  {
+    "revision": "2e81ccb1214e81821ee9",
+    "url": "/tutorialweb/static/js/runtime-main.41044198.js"
+  }
+]);
